bf42947f3473acf14e6c8fa9cbb7e5b5ef8153a8
abdulkadirmusuk <58270978+abdulkadirmusuk@users.noreply.github.com>
2021-02-21 01:40:39 +0300

    first redis app commit

---
+++ Controllers/CacheController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System.Collections.Generic;
+
+namespace Redis.Demo.Controllers
+{
+    [Route("api/[controller]")]
+    public class CacheController : ControllerBase
+    {
+        private readonly IDatabase _database;
+        public CacheController(IDatabase database)
+        {
+            _database = database;
+        }
+
+        [HttpGet]
+        public string Get([FromQuery]string key)
+        {
+            try
+            {
+                return _database.StringGet(key);
+            }
+            catch (System.Exception e)
+            {
+
+                return $"Aranan key bulunamadı: {e}";
+            }
+           
+        }
+
+        // POST: CacheController/Create
+        [HttpPost]
+        public void Post([FromBody] KeyValuePair<string,string> keyValue)
+        {
+            _database.StringSet(keyValue.Key, keyValue.Value);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Redis.Demo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Başlangıc sınıfı servisi konfigurasyonları
+            services.AddControllers();
+            //redis e burada bağlanıyoruz...
+            var redis = ConnectionMultiplexer.Connect("172.29.64.1");//local WSL Docker adress
+            services.AddScoped(s => redis.GetDatabase());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
